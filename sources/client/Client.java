6044b6c6c4c9ec0e568131325dcdfbb47bd6fcbd
konra <konradczart@interia.pl>
2018-01-11 12:14:23 +0100

    Add Server/Client connection

---
+++ sources/client/Client.java
@@ -0,0 +1,63 @@
+package client;
+
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import Message.*;
+
+
+public class Client
+{
+	private Socket socket;
+	private ServerListener serverLisener;
+	private ObjectOutputStream outStream;
+	private String localhost;
+	private int port;
+	
+
+	private static final int DEFAULT_PORT = 8189;
+	private static final String DEFAULT_HOSTNAME = "localhost";
+
+	public Client()
+	{
+		localhost = DEFAULT_HOSTNAME;
+		port = DEFAULT_PORT;
+	}
+
+	public Client(String localhost, int adress)
+	{
+		this.localhost = localhost;
+		this.port = adress;
+	}
+	public void connectServer() throws UnknownHostException, IOException
+	{
+		socket = new Socket(localhost, port);
+		outStream = new ObjectOutputStream ( socket.getOutputStream());
+	}
+
+	public void startServerListener()
+	{
+		serverLisener = new ServerListener(socket,Client.this);
+
+		Runnable r = serverLisener;
+		Thread t = new Thread(r);
+		t.start();
+	}
+
+	public void sendMessage(Message newMessage) throws IOException
+	{
+		outStream.writeObject(newMessage);
+	}
+
+	public String getLocalhost()
+	{
+		return localhost;
+	}
+
+	public int getPort()
+	{
+		return port;
+	}
+}
