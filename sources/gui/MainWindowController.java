0740b11054554844fc0c975afaa771dbe16a3003
MatCoder77 <matkimzag@wp.pl>
2018-01-12 18:53:15 +0100

    Same subcategories attributes implemented

--- sources/gui/MainWindowController.java
+++ sources/gui/MainWindowController.java
@@ -1,4 +1,5 @@
 package gui;
+
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -28,11 +29,10 @@ import javafx.stage.WindowEvent;
 public class MainWindowController {
 
 	public MainWindowController() {
-		
+
 	}
 
-	ArrayList<String> getCategories()
-	{
+	ArrayList<String> getCategories() {
 		return new ArrayList<String>(Arrays.asList("Motoryzacja", "Elektronika", "Ubrania", "Zwierzêta", "Us³ugi",
 				"Praca", "Ksi¹¿ki", "Nieruchomoœi"));
 	}
@@ -44,7 +44,7 @@ public class MainWindowController {
 		} else if (category.equals(electronicsButton.getText())) {
 			return new ArrayList<String>(Arrays.asList("Komputery", "Telefony", "Sprzêt AGD", "Telewizory"));
 		} else if (category.equals(clothesButton.getText())) {
-			return new ArrayList<String>(Arrays.asList("Ubrania", "Buty", "Bielizna", "Bi¿uteria"));
+			return new ArrayList<String>(Arrays.asList("Ubrania", "Buty", "Dodatki"));
 		} else if (category.equals(animalsButton.getText())) {
 			return new ArrayList<String>(Arrays.asList("Domowe", "Gospodarcze", "Akcesoria"));
 		} else if (category.equals(servicesButton.getText())) {
@@ -58,18 +58,65 @@ public class MainWindowController {
 		} else
 			return new ArrayList<String>(Arrays.asList("B£AD"));
 	}
-	
+
 	ArrayList<String> getCondition() {
 		return new ArrayList<String>(Arrays.asList("nowy", "u¿ywany", "dowolny"));
 	}
-	
-	ArrayList<String> getCarMakes(String subcategory) {
+
+	ArrayList<String> getCarMakes() {
 		return new ArrayList<String>(Arrays.asList("Audi", "BMW", "Chevrolet", "Citroën", "Dacia", "Daewoo", "Fiat",
 				"Ford", "Honda", "Hyundai", "Mazda", "Mercedes", "Mitsubishi", "Nissan", "Opel", "Peugeot", "Renault",
 				"Skoda", "Suzuki", "Toyota", "Volkswagen", "Pozosta³e"));
 	}
 	
+	ArrayList<String> getFuelTypes() {
+		return new ArrayList<String>(Arrays.asList("Benzyna", "Gaz"));
+	}
 	
+	ArrayList<String> getCarTypes() {
+		return new ArrayList<String>(Arrays.asList("Dostawcze", "Terenowe"));
+	}
+	
+	ArrayList<String> getMotorcycleMakes() {
+		return new ArrayList<String>(Arrays.asList("Aprilia", "Barton", "Benelli", "Beta", "Bmw", "Cagiva", "Ducati",
+				"GAS GAS", "Harley-Davidson", "Honda", "Horex", "Husqvarna", "Jawa", "Junak", "Kawasaki", "Kingway", "MZ",
+				"Romet", "Suzuki", "Victory", "Yamaha", "Zipp", "Pozosta³e"));
+	}
+	
+	ArrayList<String> getComputerManufacturers() {
+		return new ArrayList<String>(Arrays.asList("Acer", "Apple", "Asus", "Dell", "Fujitsu", "HP", "Lenovo",
+				"MSI", "Toshiba", "Pozosta³e"));
+	}
+	
+	ArrayList<String> getPhonesManufacturers() {
+		return new ArrayList<String>(Arrays.asList("Apple", "HTC", "Huawei", "LG", "Micorosoft", "Motorola", "Nokia",
+				"Samsung", "Sony", "Pozosta³e"));
+	}
+	
+	ArrayList<String> getTVManufacturers() {
+		return new ArrayList<String>(Arrays.asList("Samsung", "Sony", "Philips", "LG", "Panasonic", "Pozosta³e"));
+	}
+	
+	ArrayList<String> getClothesSizes() {
+		return new ArrayList<String>(Arrays.asList("XS", "S", "M", "L", "XL", "XXL", "XXXL"));
+	}
+	
+	ArrayList<String> getClothesTypes() {
+		return new ArrayList<String>(Arrays.asList("mêskie", "damskie"));
+	}
+	
+	ArrayList<String> getShoesSizes() {
+		return new ArrayList<String>(Arrays.asList("35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"));
+	}
+	
+	ArrayList<String> getDomesticSpecies() {
+		return new ArrayList<String>(Arrays.asList("Psy", "Koty", "Ptaki", "Króliki", "Gryzonie", "Rybki", "Pozosta³e"));
+	}
+	
+	ArrayList<String> getFarmSpecies() {
+		return new ArrayList<String>(Arrays.asList("Byd³o", "Drób", "Kozy", "Owce", "Trzoda", "Pozosta³e"));
+	}
+
 	@FXML
 	private void initialize() {
 		categoryBox.getItems().setAll(FXCollections.observableArrayList(getCategories()));
@@ -87,9 +134,12 @@ public class MainWindowController {
 
 	@FXML
 	private ComboBox<String> categoryBox;
-	
+
 	@FXML
 	private GridPane subcategoryGrid;
+
+	@FXML
+	private GridPane attributesGrid;
 
 	@FXML
 	private Button searchButton;
@@ -115,24 +165,265 @@ public class MainWindowController {
 	private Button motorisationButton;
 
 	private void updateSubcategoryPane(String category) {
-		if(!category.equals(categoryLabel.getText())) {
+		if (!category.equals(categoryLabel.getText())) {
 			ArrayList<String> subcategories = getSubcategories(category);
 			Button button;
 			subcategoryGrid.getChildren().clear();
-			//categoryLabel = new Label(motorisationButton.getText());
-			//categoryLabel.setTextAlignment(TextAlignment.CENTER);
-			//categoryLabel.setAlignment(Pos.CENTER);
-			//categoryLabel.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			// categoryLabel = new Label(motorisationButton.getText());
+			// categoryLabel.setTextAlignment(TextAlignment.CENTER);
+			// categoryLabel.setAlignment(Pos.CENTER);
+			// categoryLabel.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 			categoryLabel.setText(category);
 			subcategoryGrid.add(categoryLabel, 0, 0);
-			for(int i = 0; i < subcategories.size(); i++) {
+			for (int i = 0; i < subcategories.size(); i++) {
 				button = new Button(subcategories.get(i));
 				button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 				subcategoryGrid.add(button, 0, i + 1);
+				String subName = subcategories.get(i);
+				button.setOnAction(new EventHandler<ActionEvent>() {
+					String mySubcategory = subName;
+
+					@Override
+					public void handle(ActionEvent event) {
+						updateAttributes(mySubcategory);
+					}
+
+				});
 			}
 		}
 	}
-	
+
+	private void updateAttributes(String subcategory) {
+		if (subcategory.equals("Samochody osobowe")) {
+			ComboBox<String> makesBox = new ComboBox<>();
+			makesBox.setPromptText("marka");
+			makesBox.getItems().setAll(getCarMakes());
+			makesBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField minYear = new TextField();
+			minYear.setPromptText("rok prod. od");
+			minYear.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField maxYear = new TextField();
+			maxYear.setPromptText("rok prod. do");
+			maxYear.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField minMilage = new TextField();
+			minMilage.setPromptText("przebieg od");
+			minMilage.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField maxMilage = new TextField();
+			maxMilage.setPromptText("przebieg do");
+			maxMilage.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			ComboBox<String> fuelBox = new ComboBox<>();
+			fuelBox.setPromptText("paliwo");
+			fuelBox.getItems().setAll(FXCollections.observableArrayList(getFuelTypes()));
+			fuelBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(conditionBox, 0, 2);
+			attributesGrid.add(makesBox, 1, 0);
+			attributesGrid.add(fuelBox, 1, 1);
+			attributesGrid.add(minYear, 2, 0);
+			attributesGrid.add(maxYear, 2, 1);
+			attributesGrid.add(minMilage, 3, 0);
+			attributesGrid.add(maxMilage, 3, 1);
+			
+		}
+		else if(subcategory.equals("Samochody ciê¿arowe")) {
+			ComboBox<String> typeBox = new ComboBox<>();
+			typeBox.getItems().setAll(FXCollections.observableArrayList(getCarTypes()));
+			typeBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			typeBox.setPromptText("typ");
+			
+			TextField minYear = new TextField();
+			minYear.setPromptText("rok prod. od");
+			minYear.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField maxYear = new TextField();
+			maxYear.setPromptText("rok prod. do");
+			maxYear.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField minMilage = new TextField();
+			minMilage.setPromptText("przebieg od");
+			minMilage.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField maxMilage = new TextField();
+			maxMilage.setPromptText("przebieg do");
+			maxMilage.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			ComboBox<String> fuelBox = new ComboBox<>();
+			fuelBox.setPromptText("paliwo");
+			fuelBox.getItems().setAll(FXCollections.observableArrayList(getFuelTypes()));
+			fuelBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(conditionBox, 0, 2);
+			attributesGrid.add(typeBox, 1, 0);
+			attributesGrid.add(fuelBox, 1, 1);
+			attributesGrid.add(minYear, 2, 0);
+			attributesGrid.add(maxYear, 2, 1);
+			attributesGrid.add(minMilage, 3, 0);
+			attributesGrid.add(maxMilage, 3, 1);
+			
+		}
+		else if(subcategory.equals("Motocykle")) {
+			ComboBox<String> typeBox = new ComboBox<>();
+			typeBox.getItems().setAll(FXCollections.observableArrayList(getCarTypes()));
+			typeBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			typeBox.setPromptText("typ");
+			
+			TextField minYear = new TextField();
+			minYear.setPromptText("rok prod. od");
+			minYear.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField maxYear = new TextField();
+			maxYear.setPromptText("rok prod. do");
+			maxYear.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(typeBox, 1, 0);
+			attributesGrid.add(conditionBox, 1, 1);			
+			attributesGrid.add(minYear, 2, 0);
+			attributesGrid.add(maxYear, 2, 1);
+		}
+		else if(subcategory.equals("Czêœci")) {
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(conditionBox, 0, 2);
+		}
+		else if(subcategory.equals("Komputery")) {
+			ComboBox<String> manufacturerBox = new ComboBox<>();
+			manufacturerBox.getItems().setAll(FXCollections.observableArrayList(getComputerManufacturers()));
+			manufacturerBox.setPromptText("producent");
+			manufacturerBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(manufacturerBox, 1, 0);
+			attributesGrid.add(conditionBox, 1, 1);
+		}
+		else if(subcategory.equals("Telefony")) {
+			ComboBox<String> manufacturerBox = new ComboBox<>();
+			manufacturerBox.getItems().setAll(FXCollections.observableArrayList(getPhonesManufacturers()));
+			manufacturerBox.setPromptText("producent");
+			manufacturerBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(manufacturerBox, 1, 0);
+			attributesGrid.add(conditionBox, 1, 1);
+		}
+		else if(subcategory.equals("Sprzêt AGD")) {
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(conditionBox, 0, 2);
+		}
+		else if(subcategory.equals("Telewizory")) {
+			ComboBox<String> manufacturerBox = new ComboBox<>();
+			manufacturerBox.getItems().setAll(FXCollections.observableArrayList(getTVManufacturers()));
+			manufacturerBox.setPromptText("producent");
+			manufacturerBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField minSize = new TextField();
+			minSize.setPromptText("przek¹tna od");
+			minSize.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			TextField maxSize = new TextField();
+			maxSize.setPromptText("przek¹tna do");
+			maxSize.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(manufacturerBox, 1, 0);
+			attributesGrid.add(conditionBox, 1, 1);
+			attributesGrid.add(minSize, 2, 0);
+			attributesGrid.add(maxSize, 2, 1);
+		}
+		else if(subcategory.equals("Ubrania")) {
+			ComboBox<String> sizeBox = new ComboBox<>();
+			sizeBox.getItems().setAll(FXCollections.observableArrayList(getClothesSizes()));
+			sizeBox.setPromptText("rozmiar");
+			sizeBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			ComboBox<String> typeBox = new ComboBox<>();
+			typeBox.getItems().setAll(FXCollections.observableArrayList(getClothesTypes()));
+			typeBox.setPromptText("rodzaj");
+			typeBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(conditionBox, 0, 2);
+			attributesGrid.add(sizeBox, 1, 0);
+			attributesGrid.add(typeBox, 1, 1);
+		}
+		else if(subcategory.equals("Buty")) {
+			ComboBox<String> sizeBox = new ComboBox<>();
+			sizeBox.getItems().setAll(FXCollections.observableArrayList(getShoesSizes()));
+			sizeBox.setPromptText("rozmiar");
+			sizeBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			ComboBox<String> typeBox = new ComboBox<>();
+			typeBox.getItems().setAll(FXCollections.observableArrayList(getClothesTypes()));
+			typeBox.setPromptText("rodzaj");
+			typeBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(conditionBox, 0, 2);
+			attributesGrid.add(sizeBox, 1, 0);
+			attributesGrid.add(typeBox, 1, 1);
+		}
+		else if(subcategory.equals("Dodatki")) {
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(conditionBox, 0, 2);
+		}
+		else if(subcategory.equals("Domowe")) {
+			ComboBox<String> speciesBox = new ComboBox<>();
+			speciesBox.getItems().setAll(FXCollections.observableArrayList(getDomesticSpecies()));
+			speciesBox.setPromptText("gatunek");
+			speciesBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(speciesBox, 0, 2);
+		}
+		else if(subcategory.equals("Gospodarcze")) {
+			ComboBox<String> speciesBox = new ComboBox<>();
+			speciesBox.getItems().setAll(FXCollections.observableArrayList(getFarmSpecies()));
+			speciesBox.setPromptText("gatunek");
+			speciesBox.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(speciesBox, 0, 2);
+		}
+		else if(subcategory.equals("Akcesoria")) {
+			attributesGrid.getChildren().clear();
+			attributesGrid.add(minPrice, 0, 0);
+			attributesGrid.add(maxPrice, 0, 1);
+			attributesGrid.add(conditionBox, 0, 2);
+		}
+	}
+
 	@FXML
 	private void motorisationButtonActivated(ActionEvent event) {
 		// MOTORYZACJA
--- sources/gui/SignInOrRegisterController2.java
+++ sources/gui/SignInOrRegisterController2.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -14,6 +16,50 @@ public class SignInOrRegisterController2 {
 	@FXML
 	private Button logInButton;
 	
+	@FXML
+	private Button registerMeButton;
+	
+	@FXML
+	private TextField nameField;
+	
+	@FXML
+	private TextField surnameField;
+	
+	@FXML
+	private TextField phoneNumberField;
+	
+	@FXML
+	private TextField loginField;
+	
+	@FXML
+	private PasswordField passwordField;
+	
+	@FXML
+	private PasswordField password2Field;
+	
+	String getName() {
+		return nameField.getText();
+	}
+	
+	String getSurname() {
+		return surnameField.getText();
+	}
+	
+	String getPhoneNumber() {
+		return phoneNumberField.getText();
+	}
+	
+	String getLogin() {
+		return loginField.getText();
+	}
+	
+	String getPassword() {
+		return passwordField.getText();
+	}
+	
+	String getPassword2() {
+		return password2Field.getText();
+	}
 
 	@FXML
 	void handleLogInButtonActive(ActionEvent event) {
@@ -32,6 +78,13 @@ public class SignInOrRegisterController2 {
 		// SignInOrRegisterController controller =
 		// loader.<SignInOrRegisterController>getController();
 		stage.show();
+
+	}
+	
+	@FXML
+	void handleRegisterMeButtonActive(ActionEvent event) {
+
+		//NACIŒNIETO ZAREJESTRUJ (ten na dole)
 
 	}
 }
