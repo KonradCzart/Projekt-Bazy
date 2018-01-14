c90a7ef0c9f1ea465fbe7fabf42c0dc21314b53e
konra <konradczart@interia.pl>
2018-01-14 01:45:42 +0100

    Add insertAttribute sql and method to add

---
+++ databaseSql/InsertAtribut.sql
@@ -0,0 +1,172 @@
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Audi");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "BMW");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Chevrolet");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Citroën");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Dacia");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Daewoo");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Fiat");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Ford");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Honda");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Hyundai");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Mazda");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Mercedes");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Mitsubishi");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Nissan");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Opel");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Peugeot");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Renault");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Skoda");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Suzuki");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Toyota");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Volkswagen");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Pozostałe");
+
+INSERT INTO attributes (Name, Value) VALUES ( "paliwo" , "Benzyna");
+INSERT INTO attributes (Name, Value) VALUES ( "paliwo" , "Dissel");
+
+INSERT INTO attributes (Name, Value) VALUES ( "typ" , "Dostawcze");
+INSERT INTO attributes (Name, Value) VALUES ( "typ" , "Terenowe");
+
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Aprilia");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Barton");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Benelli");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Beta");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Bmw");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Cagiva");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Ducati");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "GAS GAS");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Harley-Davidson");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Honda");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Horex");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Husqvarna");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Jawa");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Junak");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Kawasaki");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Kingway");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "MZ");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Romet");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Suzuki");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Victory");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Yamaha");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Zipp");
+INSERT INTO attributes (Name, Value) VALUES ( "marka" , "Pozostałe");
+
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Acer");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Apple");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Asus");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Dell");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Fujitsu");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "HP");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Lenovo");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "MSI");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Toshiba");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Pozostałe");
+
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Apple");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "HTC");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Huawei");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "LG");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Micorosoft");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Motorola");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Nokia");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Samsung");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Sony");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Pozostałe");
+
+
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Samsung");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Sony");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Philips");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "LG");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Panasonic");
+INSERT INTO attributes (Name, Value) VALUES ( "producent" , "Pozostałe");
+
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "XS");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "S");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "M");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "L");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "XL");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "XXL");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "XXXL");
+
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "męskie");
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "damskie");
+
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "35");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "36");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "37");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "38");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "39");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "40");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "41");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "42");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "43");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "44");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "45");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "46");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "47");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "48");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "49");
+INSERT INTO attributes (Name, Value) VALUES ( "rozmiar" , "50");
+
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Psy");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Koty");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Ptaki");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Króliki");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Gryzonie");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Rybki");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Pozostałe");
+
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Bydło");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Drób");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Kozy");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Owce");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Trzoda");
+INSERT INTO attributes (Name, Value) VALUES ( "gatunek" , "Pozostałe");
+
+
+INSERT INTO attributes (Name, Value) VALUES ( "język" , "Angielski");
+INSERT INTO attributes (Name, Value) VALUES ( "język" , "Francuski");
+INSERT INTO attributes (Name, Value) VALUES ( "język" , "Niemiecki");
+INSERT INTO attributes (Name, Value) VALUES ( "język" , "Rosyjski");
+INSERT INTO attributes (Name, Value) VALUES ( "język" , "Inne");
+
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "Młodzieżowe");
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "Przyrodnicze");
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "Informatyczne");
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "Popularnonaukowe");
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "Kulinarne");
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "Motoryzacyjne");
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "Erotyczne");
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj" , "Inne");
+
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj oferty" , "Sprzedaż");
+INSERT INTO attributes (Name, Value) VALUES ( "rodzaj oferty" , "Wynajem");
+
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "poniżej 50 tys");
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "50 - 150 tys");
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "150 - 250 tys");
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "powyżej 250 tys");
+
+INSERT INTO attributes (Name, Value) VALUES ( "przebieg" , "poniżej 500 m²");
+INSERT INTO attributes (Name, Value) VALUES ( "przebieg" , "500 - 1000 m²");
+INSERT INTO attributes (Name, Value) VALUES ( "przebieg" , "1000 - 2000 m²");
+INSERT INTO attributes (Name, Value) VALUES ( "przebieg" , "powyżej 2000 m²");
+
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "poniżej 50 m²");
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "50 - 100 m²");
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "100 - 200 m²");
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "powyżej 200 m²");
+
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "poniżej 25 m²");
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "25 - 50 m²");
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "50 - 100 m²");
+INSERT INTO attributes (Name, Value) VALUES ( "powierzchnia" , "powyżej 100 m²");
+
+
+INSERT INTO attributes (Name, Value) VALUES ( "przekątna" , "poniżej 30");
+INSERT INTO attributes (Name, Value) VALUES ( "przekątna" , "30 - 40");
+INSERT INTO attributes (Name, Value) VALUES ( "przekątna" , "40 - 50");
+INSERT INTO attributes (Name, Value) VALUES ( "przekątna" , "powyżej 50");
+	
+
